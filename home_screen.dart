fd300d53de6edd3c4758f8a22b14f7770f8dd10b
류준호 <90046151+RyuJunho@users.noreply.github.com>
2023-09-15 19:09:12 +0900

    Add files via upload

---
+++ home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: const Center(
+          child: Text(
+            "오늘의 웹툰",
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
